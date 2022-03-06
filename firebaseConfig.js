175828c85cdb3ba65293ba4af85cc758e5d295c4
Emre <pi.emree@gmail.com>
2022-03-06 19:59:27 +0300

    env confg

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,13 @@
 
 export default {
-    apiKey: "AIzaSyAa4qNa8oO7kxDDM8mtGBcp-XV0TWrXfaE",
-    authDomain:"public-manager.firebaseapp.com" ,
-    projectId:"public-manager",
-    storageBucket:"public-manager.appspot.com",
-    messagingSenderId:"962352157944",
-    appId:"1:962352157944:web:71ce1d68d583585b38d70d" ,
+  apiKey: process.env.firebaseApiKey,
+  authDomain:process.env.firebaseAuthDomain ,
+  databaseURL:process.env.firebaseDbUrl ,
+  projectId:process.env.firebaseProjectId,
+  storageBucket:process.env.firebaseStorageBucket,
+  messagingSenderId:process.env.firebaseMessagingSenderId,
+  appId:process.env.firebaseAppId ,
+  measurementId:process.env.firebaseMeasurementId
   };
 
 
